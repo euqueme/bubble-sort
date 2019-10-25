bf6b7591418b5e7ce3486b5d29ee7c4cd98e34e6
Santiago Guerra <guerrasantiago19@gmail.com>
2019-10-25 12:40:44 -0500

    Rubocop test fix ruby issues

--- main.rb
+++ main.rb
@@ -2,6 +2,9 @@
 
 def bubble_sort(arr)
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> 116bfe9... RUBOCOP TEST
   len = arr.size - 1
   len.times do |i|
     left = arr[i]
@@ -9,6 +12,7 @@ def bubble_sort(arr)
     if (left <=> right) == 1
       arr[i + 1] = left
       arr[i] = right
+<<<<<<< HEAD
     end
   end
   arr == arr.sort ? arr : bubble_sort(arr)
@@ -32,27 +36,41 @@ def bubble_sort_by(arr)
         arr[i+1] = left 
         arr[i] = right 
       end
+=======
+>>>>>>> 116bfe9... RUBOCOP TEST
     end
-  arr == arr.sort ?  arr : bubble_sort(arr)
+  end
+  arr == arr.sort ? arr : bubble_sort(arr)
 end
 
-def bubble_sort_by arr
-  len = arr.size-1
+def bubble_sort_by(arr)
+  len = arr.size - 1
   while len >= 0
     len.times do |i|
       left = arr[i]
+<<<<<<< HEAD
       right = arr[i+1]
       if (yield(left, right)) > 0
         arr[i+1] = left 
         arr[i] = right 
 >>>>>>> 112907a... ADD RUBOCOP, EDIT README
+=======
+      right = arr[i + 1]
+      if yield(left, right).positive?
+        arr[i + 1] = left
+        arr[i] = right
+>>>>>>> 116bfe9... RUBOCOP TEST
       end
     end
     len -= 1
   end
 <<<<<<< HEAD
+<<<<<<< HEAD
   arr
 =======
   return arr
 >>>>>>> 112907a... ADD RUBOCOP, EDIT README
+=======
+  arr
+>>>>>>> 116bfe9... RUBOCOP TEST
 end
